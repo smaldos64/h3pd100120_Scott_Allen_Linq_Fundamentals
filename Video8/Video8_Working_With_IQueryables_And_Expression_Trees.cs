614f353526d24d53915ae9287c541411aba37644
smaldos64 <gudumholm34@gmail.com>
2020-08-17 11:29:40 +0200

    Adderet flere eksempler med Expression og Func.

--- Video8/Video8_Working_With_IQueryables_And_Expression_Trees.cs
+++ Video8/Video8_Working_With_IQueryables_And_Expression_Trees.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Scott_Allen_Linq_Fundamentals.Context;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -9,6 +11,8 @@ namespace Scott_Allen_Linq_Fundamentals.Video8
     {
         public static void Video8_Working_With_IQueryables_And_Expression_Trees_Start()
         {
+            var db = new DatabaseContext();
+
             // Når man arbejder op imod Entity Framework benytter man sig af (kan man benytte sig af) 
             // et nýt interface => IQueryable. Derfor kan man nu benytte sig af 4 forskellige
             // versioner af Where metoden (når man ikke arbejder op imod Entity Framework
@@ -20,10 +24,14 @@ namespace Scott_Allen_Linq_Fundamentals.Video8
             // C# compileren tager pr. default en af versionerne, der returnerer en IQueryable !!!
 
             // IEnumerable skal udføre kode i memory.
-            // IQuerybale behøver ikke udføre kode i mmemory !!!
 
+            // IQuerybale behøver ikke udføre kode i memory !!!
             // Ved IQueryable er det den overførte Expression, der afgør hvordan Linq udtrykket
             // udføres !!!
+
+            // Det vil sige, at modsat et udtryk der returnerer IEnumerable, returneres der nu
+            // med IQuerable et Expression der wrapper en Func af Cars og bool : 
+            // Expression<Func<Car,bool>>. Ved IEnumerable returneres der en Func af Cars og bool. 
 
             Console.WriteLine("");
             Console.WriteLine("Video8_Working_With_IQueryables_And_Expression_Trees");
@@ -33,15 +41,32 @@ namespace Scott_Allen_Linq_Fundamentals.Video8
             Func<int, int> square = x => x * x;
             Func<int, int, int> add = (x, y) => x + y;
             Expression<Func<int, int, int>> addExpression = (x, y) => x + y; 
+            // Når vi wrapper et FUnc udtryk med Expression har vi den situation der opstår,
+            // når vi anvender Where metoden på en IQueryable, hvor Where jo i henhold til
+            // ovennævnte tager imod et Expression af Func<Car,bool>.
             // Kræver namespace System.Linq.Expression
 
-            var result = add(3, 5);
-            Console.WriteLine(result);
+            //var result = add(3, 5);
             Console.WriteLine(add);
-
+            Console.WriteLine(add(3, 5));
+            Console.WriteLine(add.Invoke(3, 5));
+            //Console.WriteLine(result);
+            
+            var addExpressionCompile = addExpression.Compile();
+            Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine(addExpression);
+            Console.WriteLine(addExpressionCompile);
+            Console.WriteLine(addExpressionCompile.Invoke(6, 8));
+            Console.WriteLine(addExpressionCompile(6, 8));
+            Console.WriteLine(addExpression.Compile()(6, 8));
+            Console.WriteLine(addExpression.Compile().Invoke(6, 8));
             //result = addExpression.Body(4, 8);
+
+            //var queryMethodSyntax = db.Cars.
+            //    OrderByDescending(c => c.Combined).
+            //    ThenBy(c => c.Name).
+            //    Take(10);
         }
     }
 }
